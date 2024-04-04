ce3b1169f932f40d91c174c1e4502fcd7873574d
ValeriiDroniak <droniak.valerii@gmail.com>
2024-04-04 15:11:19 +0300

    finished task4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,64 @@
+class Key {
+ private signature: number;
+
+ constructor() {
+	this.signature = Math.random();
+ }
+
+ getSignature(): number {
+	return this.signature;
+ }
+}
+
+class Person {
+	private key: Key;
+
+	constructor( key: Key) {
+		this.key = key;
+	}
+
+	getKey(): Key {
+		return this.key;
+	}
+}
+
+abstract class House  {
+	public door: boolean = false;
+	public key: Key;
+	public tenants: Person[] = [];
+
+	constructor(key: Key) {
+		this.key = key;
+		// this.comeIn(new Person(key));
+	}
+
+	public comeIn(person: Person): void {
+		if(this.door) {
+			this.tenants.push(person)
+			console.log('Added resident to house.');
+		} else {
+			console.log('The door is closed. Unable to log in.');
+		}
+	}
+
+	public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+	constructor(key: Key){
+		super(key)
+	}
+
+	public openDoor(key: Key): void {
+		if(this.key.getSignature() === key.getSignature()) {
+			this.door = true;
+			console.log('Door opened successfully.')
+		} else {
+      console.log("Cannot open the door. Invalid key.");
+    }
+	}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
